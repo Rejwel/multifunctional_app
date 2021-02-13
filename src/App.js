da076b157fc4aef25f7ffef46fe6f802454eb134
Pawel Dera <236517@edu.p.lodz.pl>
2021-02-13 18:09:19 +0100

    First commit with calculator

---
+++ src/App.js
@@ -0,0 +1,208 @@
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import CalcFrame from './CalcFrame'
+import { unstable_concurrentAct } from 'react-dom/test-utils';
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            typed: false,
+            result: 0,
+            savedResult: 0,
+            numbers: Array.from(Array(10).keys()),
+            functions: ['C','+','-','*','/', '=', '.', '√', 'x^n', '◄'],
+            currentFunction: 0,
+            startNumber: true,
+            dotUsed: false
+        };
+    }
+
+    Add(i) {
+        this.setState({typed: true});
+        if(this.state.result.length < 50 || this.state.result.length === undefined) {
+            this.state.startNumber ? 
+            this.setState({result: this.state.numbers[i], startNumber: false}) : 
+            this.setState({result: this.state.result +  '' + this.state.numbers[i]});
+        }
+    }
+
+    Calculate = (i, a, b) => {
+        if(i === 1) return (parseFloat(a) + parseFloat(b));
+        if(i === 2) return (parseFloat(a) - parseFloat(b));
+        if(i === 3) return (parseFloat(a) * parseFloat(b));
+        if(i === 4 && b != 0) return (parseFloat(a) / parseFloat(b)).toFixed(5);
+        if(i === 7) return (Math.sqrt(parseFloat(b))).toFixed(5);
+        if(i === 8) return (Math.pow(parseFloat(a), parseInt(b))).toFixed(5);
+        return 0;
+    }
+
+    ReturnResult = (i) => {
+        this.state.result !== 0 && this.state.savedResult !==0 ?
+                this.setState({
+                    savedResult: this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result),
+                    currentFunction: i,
+                    result: 0,
+                    dotUsed: false,
+                    startNumber: true,
+                    typed: false,
+                }) :
+                this.state.typed ?
+                this.setState({
+                    currentFunction: i,
+                    savedResult: this.state.result,
+                    result: 0,
+                    dotUsed: false,
+                    startNumber: true,
+                    typed: false,
+                }) :
+                this.setState({
+                    currentFunction: i,
+                });
+    }
+
+    Option(i) {
+        switch (i) {
+            // clearing function
+            case 0:
+                this.state.startNumber && this.setState({savedResult: 0, currentFunction: 0, typed: false, dotUsed: false});
+                this.setState({startNumber: true, result: 0, typed: false, dotUsed: false});
+                break;
+
+            // add
+            case 1:
+                this.ReturnResult(i);
+                break;
+
+            // sub
+            case 2:
+                this.ReturnResult(i);
+                break;
+
+            // mult
+            case 3:
+                this.ReturnResult(i);
+                break;
+
+            // div
+            case 4:
+                this.ReturnResult(i);
+                break;
+
+                
+            // dot
+            case 6:
+                !this.state.dotUsed && this.setState({result: this.state.result + "."})
+                this.setState({dotUsed: true})
+                break;
+
+            // sqrt
+            case 7:
+                this.setState({
+                    result: parseFloat(this.Calculate(i, this.state.savedResult, this.state.result))
+                });
+                break;
+
+            // pow
+            case 8:
+                this.ReturnResult(i);
+                break;
+            
+            // delNum
+            case 9:
+
+                this.state.result > 0 && this.state.result.toString().slice(0, -1) !== '' ?
+                this.setState({
+                    result: this.state.result.toString().slice(0, -1),
+                }) :
+                this.setState({
+                    startNumber: true,
+                    result: 0,
+                });
+
+                this.state.result[this.state.result.length-1] === '.' &&
+                this.setState({ dotUsed: false });
+                
+                break;
+
+            case 5:
+                    switch (this.state.currentFunction) {
+                        case 1:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        case 2:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        case 3:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        case 4:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        case 7:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        case 8:
+                            this.setState({
+                                result: parseFloat(this.Calculate(this.state.currentFunction, this.state.savedResult, this.state.result)),
+                                savedResult: 0,
+                                currentFunction: 0
+                            });
+                            break;
+
+                        default:
+                            break;
+                    }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    render()
+    {
+        return(
+            <CalcFrame 
+                typed={this.state.typed}
+                currentFunction={this.state.currentFunction}
+                savedResult={this.state.savedResult}
+                result={this.state.result}
+                onClick={(i) => this.Add(i)}
+                onClickFunction={(i) => this.Option(i)}
+                numbers={this.state.numbers}
+                startNumber={this.state.startNumber}
+                functions={this.state.functions}
+            />
+        );
+    }   
+}
+
+export default App
---
+++ src/CalcFrame.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './CalcFrame.css'
+import ReactHtmlParser from 'react-html-parser'
+
+function Number(props) {
+    const className = (props.numbers === 0 ? "finalNumber" : "number")
+    return (
+        <button className={className} onClick={props.onClick}>
+            {props.numbers}
+        </button>
+    );
+}
+
+function Result(props) {
+    let fun, savedResult;
+    props.currentFunction === 0 ? fun = "" : fun = props.functions[props.currentFunction];
+    props.savedResult === 0 ? savedResult = "" : savedResult = props.savedResult;
+    return (
+        <button className="result">
+            <div className="result_inner_top">{savedResult} {fun}</div>
+            <div className="result_inner_bottom">{props.result}</div>
+        </button>
+    );
+}
+
+function Calculation(props) {
+    const fun = `x<sup>n</sup>`;
+    return (
+        <button className="number" onClick={props.onClickFunction}>
+            {props.currentFunction === 8 ? ReactHtmlParser(fun) : props.functions}
+        </button>
+    );
+}
+
+class CalcFrame extends React.Component {
+
+    renderNumber(i) {
+        return(
+            <Number
+                numbers={this.props.numbers[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
+    renderResult() {
+        return (
+            <Result
+                functions={this.props.functions}
+                currentFunction={this.props.currentFunction}
+                savedResult={this.props.savedResult}
+                result={this.props.result}
+                startNumber={this.props.startNumber}
+            />
+        )
+    }
+
+    renderFunction(i) {
+        return (
+            <Calculation
+                currentFunction={i}
+                functions={this.props.functions[i]}
+                onClickFunction={() => this.props.onClickFunction(i)}
+            />
+        )
+    }
+
+
+    /* 
+        0 - clear
+        1 - add
+        2 - sub
+        3 - mult
+        4 - div
+        5 - return
+        6 - dot
+        7 - radical
+        8 - pow
+        9 - delNum
+    */
+    
+    render() {
+        return(
+            <div className="container">
+                <div className="calculator">
+                    <div className="row">
+                        {this.renderResult()}
+                    </div>
+                    <div className="row">
+                        {this.renderNumber(7)}
+                        {this.renderNumber(8)}
+                        {this.renderNumber(9)}
+                        {this.renderFunction(8)}
+                        {this.renderFunction(7)}
+                        {this.renderFunction(9)}
+                    </div>
+                    <div className="row">
+                        {this.renderNumber(4)}
+                        {this.renderNumber(5)}
+                        {this.renderNumber(6)}
+                        {this.renderFunction(2)}
+                        {this.renderFunction(1)}
+                        {this.renderFunction(0)}
+                    </div>
+                    <div className="row">
+                        {this.renderNumber(1)}
+                        {this.renderNumber(2)}
+                        {this.renderNumber(3)}
+                        {this.renderFunction(3)}
+                        {this.renderFunction(4)}
+                        {this.renderFunction(0)}
+                    </div>
+                    <div className="row">
+                        {this.renderNumber(0)}
+                        {this.renderFunction(6)}
+                        {this.renderFunction(5)}
+                        {this.renderFunction(0)}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CalcFrame
+
+
